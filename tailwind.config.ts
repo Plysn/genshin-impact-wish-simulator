d42afdbbdbbc46de623f2b0d343c846cdcf1f937
Plysn <phuonglynguyen61@gmail.com>
2025-06-01 23:12:44 +0700

    main - feat: add font gi-global

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,14 +2,7 @@ import type { Config } from 'tailwindcss';
 
 const config: Config = {
   content: ['./index.html', './src/**/*.{ts,tsx}'],
-  theme: {
-    extend: {
-      fontFamily: {
-        'genshin-icon': ['GenshinIcon'],
-      },
-    },
-  },
-  plugins: [],
+  plugins: []
 };
 
 export default config;
